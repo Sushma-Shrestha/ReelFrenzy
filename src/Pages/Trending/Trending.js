20e53623e81fa4b0fe7a3941529ee6eba210f233
Sushma Shrestha <96963817+Sushma-Shrestha@users.noreply.github.com>
2023-02-14 12:35:25 +0545

    Trending movies and series

---
+++ src/Pages/Trending/Trending.js
@@ -0,0 +1,51 @@
+import axios from "axios";
+import "./Trending.css";
+import { useEffect, useState } from "react";
+import SingleContent from "../../components/SingleContent/SingleContent";
+import CustomPagination from "../../components/Pagination/CustomPagination";
+
+const Trending = () => {
+  const [page, setPage] = useState(1);
+  const [content, setContent] = useState([]);
+  const [totalPages, setTotalPages] = useState(0);
+
+  const fetchTrending = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}&page=${page}`
+    );
+    setContent(data.results);
+    const checkpage = async() => await axios.get(
+      `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}`
+    ).then((res) => {  setTotalPages(res.data.total_pages); }).catch((err) => { console.log(err); });  
+    checkpage();
+  };
+
+  useEffect(() => {
+    window.scroll(0, 0);
+    fetchTrending();
+    // eslint-disable-next-line
+  }, [page]);
+
+  return (
+    <div>
+      <span className="pageTitle">Trending Today</span>
+      <div className="trending">
+        {content &&
+          content.map((c) => (
+            <SingleContent
+              key={c.id}
+              id={c.id}
+              poster={c.poster_path}
+              title={c.title || c.name}
+              date={c.first_air_date || c.release_date}
+              media_type={c.media_type}
+              vote_average={c.vote_average}
+            />
+          ))}
+      </div>
+      <CustomPagination setPage={setPage} numOfPages={totalPages}/>
+    </div>
+  );
+};
+
+export default Trending;
